511fd90a771384156549137d39870be45d2f1e44
pedroabp <pedroabp5@gmail.com>
2010-07-21 20:03:26 -0500

    Formulario para informacion tecnico-operativa de acueducto

---
+++ apps/arc_portable/modules/acueducto_tecnicooperativa/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * acueducto_tecnicooperativa actions.
+ *
+ * @package    arc
+ * @subpackage acueducto_tecnicooperativa
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class acueducto_tecnicooperativaActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+//    $this->forward('default', 'module');
+  }
+}
---
+++ apps/arc_portable/modules/acueducto_tecnicooperativa/templates/indexSuccess.php
@@ -0,0 +1,7 @@
+<?php echo javascript_include_tag('acueducto/tecnicooperativa/form_acu_tecnicooperativa.js'); ?>
+<script>
+            Ext.onReady(function(){
+              tecnicooperativaacueducto_form.render(Ext.get("div_tecnicooperativaacueducto"));
+            });
+</script>
+<div id="div_tecnicooperativaacueducto"></div>
