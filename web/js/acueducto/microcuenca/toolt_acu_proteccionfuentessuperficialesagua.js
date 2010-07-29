d566463288c5b8110a13446cf8dcc22996c79238
unknown <maryit@.(none)>
2010-07-28 22:38:02 -0500

    editando un tooltip de proteccion de las fuentes

--- web/js/acueducto/microcuenca/toolt_acu_proteccionfuentessuperficialesagua.js
+++ web/js/acueducto/microcuenca/toolt_acu_proteccionfuentessuperficialesagua.js
@@ -1,15 +1,13 @@
-
-var ayuda_acu_pfu_programas_aislamiento  = 'existe el programa de aislamiento??, selecione esta casilla en caso de que exista';
-var ayuda_acu_pfu_programas_reforestacion  = '';
-var ayuda_acu_pfu_programas_mejoramiento_uso_suelo  = '';
-var ayuda_acu_pfu_programas_adquisicion_tierra  = '';
-var ayuda_acu_pfu_programas_educacion_ambiental  = '';
-var ayuda_acu_pfu_programas_reduccion_uso_agroquimicos  = '';
-var ayuda_acu_pfu_cobertura_forestal_area_protectora_cauce  = '';
-var ayuda_acu_pfu_aislamiento_franja_amarilla_microcuenca  = '';
-var ayuda_acu_pfu_aislamiento_franja_amarilla_microcuenca  = '';
-var ayuda_acu_pfu_area_microcuenca_propiedad_acueducto  = '';
-var ayuda_acu_pfu_extension_has  = 'escriba el numero de ext has';
-var ayuda_acu_pfu_situacion_arriba_captacion_reserva  = '';
+var ayuda_acu_pfu_programas_aislamiento  = 'Existe el programa de aislamiento??, selecione esta casilla en caso de que exista';
+var ayuda_acu_pfu_programas_reforestacion  = 'Existe el programa de reforestación, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_programas_mejoramiento_uso_suelo  = 'Existe el programa de mejoramiento uso suelo, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_programas_adquisicion_tierra  = 'Existe el programa de adquisicion de tierras, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_programas_educacion_ambiental  = 'Existe el programa de educacion ambiental, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_programas_reduccion_uso_agroquimicos  = 'Existe el programa de reduccion de uso de agroquimicos, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_cobertura_forestal_area_protectora_cauce  = 'Existe area protectora de cauce, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_aislamiento_franja_amarilla_microcuenca  = 'Existe franja amirilla, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_area_microcuenca_propiedad_acueducto  = 'Existe area de las microcuencas que sea propiedad del acueducto, seleccion esta casilla en caso de que exista';
+var ayuda_acu_pfu_extension_has  = 'Escriba el numero de ext has';
+var ayuda_acu_pfu_situacion_arriba_captacion_reserva  = 'Es aquella protegida porque la ley asi lo indica';
 var ayuda_acu_pfu_situacion_arriba_captacion_protegida='Es aquella protegida por iniciativa de los propietarios, comunidad o por autoridades locales';  
   					
