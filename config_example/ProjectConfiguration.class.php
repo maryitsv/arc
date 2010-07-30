a83d1a343598f7c7d2ce658bbe40d64810775ae1
pedroabp <pedroabp5@gmail.com>
2010-07-29 21:21:30 -0500

    Configuracion de Git para ignorar las carpetas 'lib/model/map' y 'lib/model/om'. Regeneracion del modelo

--- config_example/ProjectConfiguration.class.php
+++ config_example/ProjectConfiguration.class.php
@@ -4,35 +4,23 @@ require_once 'C:\\xampp\\htdocs\\symfony\\lib/autoload/sfCoreAutoload.class.php'
 //require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
 
 //$path = '../../php/PEAR/symfony/autoload/sfCoreAutoload.class.php';
-<<<<<<< HEAD:config_example/ProjectConfiguration.class.php
 //$path = '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
 $path = 'C:\Users\pedroabp\Desktop\xampplite\php\PEAR\symfony\autoload\sfCoreAutoload.class.php';
-=======
-
-$path = 'C:\\xampp\\htdocs\\symfony\\lib/autoload/sfCoreAutoload.class.php';
-
-//$path = '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
-
->>>>>>> beec9d48f8ce3337afc9b1e1e83dc97e88e69a09:config/ProjectConfiguration.class.php
 
 /*
-if(basename(getcwd())=='web') {
-  $path = '../'.$path;
-} 
-*/
-<<<<<<< HEAD:config_example/ProjectConfiguration.class.php
-
+ if(basename(getcwd())=='web') {
+ $path = '../'.$path;
+ }
+ */
 require_once $path;
-=======
-//require_once $path;
->>>>>>> beec9d48f8ce3337afc9b1e1e83dc97e88e69a09:config/ProjectConfiguration.class.php
+
 
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
-  public function setup()
-  {
-    $this->enablePlugins('sfPropelPlugin');
-  }
+	public function setup()
+	{
+		$this->enablePlugins('sfPropelPlugin');
+	}
 }
